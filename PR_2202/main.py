c9833e19a423ab1173dc37b1ad5160ffe42528b1
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2021-03-01 06:18:10 +0300

    upd

--- PR_2202/main.py
+++ PR_2202/main.py
@@ -16,9 +16,15 @@ class CircleAnimate(QtWidgets.QMainWindow):
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_values)
-        self.timer.start(33)
+        self.timer.start(10)
 
     def paintEvent(self, event):
+        self.draw_notation(r=200, b=200, g=200,
+                           center_x=255, center_y=240, radius=200,
+                           angle=6, radius_in=5)
+        self.draw_notation(r=0, b=0, g=0,
+                           center_x=255, center_y=240, radius=200,
+                           angle=30, radius_in=5)
         self.draw_gear(r=66, b=133, g=244,
                        center_x=255, center_y=245,
                        radius=75, to_left=True)
@@ -32,13 +38,13 @@ class CircleAnimate(QtWidgets.QMainWindow):
                        center_x=255, center_y=240,
                        radius=25, to_left=False)
         self.draw_arrow(r=100, b=100, g=100,
-                        x=255, y=240, w=200, h=5,
+                        x=255, y=240, w=5, h=200,
                         radius=10)
         self.draw_arrow(r=150, b=150, g=150,
-                        x=255, y=240, w=150, h=10,
+                        x=255, y=240, w=10, h=150,
                         radius=60)
         self.draw_arrow(r=200, b=200, g=200,
-                        x=255, y=240, w=100, h=15,
+                        x=255, y=240, w=15, h=100,
                         radius=60*12)
 
     def draw_arrow(self, r, b, g, x, y, w, h, radius):
@@ -51,7 +57,7 @@ class CircleAnimate(QtWidgets.QMainWindow):
         qp.setTransform(transform)
         qp.setPen(QtGui.QColor(0, 0, 0, 0))
         qp.setBrush(QtGui.QColor(r, b, g))
-        qp.drawRect(0, h / -2, w, h)
+        qp.drawRect(w / -2, 0, w, -h)
         qp.end()
 
     def draw_gear(self, r, b, g, center_x, center_y, radius, to_left):
@@ -96,6 +102,20 @@ class CircleAnimate(QtWidgets.QMainWindow):
         path.addText(radius * -1.31, radius * 0.67, QtGui.QFont('Georgia', 1.45 * radius), "➕")
         qp.drawPath(path)
         qp.end()
+
+    def draw_notation(self, r, b, g, center_x, center_y, radius, angle, radius_in):
+        transform = QtGui.QTransform(1, 0, 0, 1, center_x, center_y)
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing)
+        qp.setTransform(transform)
+        qp.setPen(QtGui.QColor(0, 0, 0, 0))
+        qp.setBrush(QtGui.QColor(r, b, g))
+        for alpha in range(0, 360, angle):
+            x = radius * np.cos(np.radians(alpha) - np.pi / 2)
+            y = radius * np.sin(np.radians(alpha) - np.pi / 2)
+            qp.drawEllipse(x, y, radius_in, radius_in)
+
 
     def update_values(self):
         self.rotate += 1
