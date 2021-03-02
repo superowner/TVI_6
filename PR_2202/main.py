3e5b1f05449b199ac9da2babd74b6446d318fe64
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2021-03-02 16:45:08 +0300

    upd

--- PR_2202/main.py
+++ PR_2202/main.py
@@ -11,7 +11,6 @@ class CircleAnimate(QtWidgets.QMainWindow):
         super().__init__()
         self.resize(510, 480)
         self.qp = QtGui.QPainter()
-
         self.rotate = 0
 
         self.timer = QtCore.QTimer()
@@ -19,109 +18,113 @@ class CircleAnimate(QtWidgets.QMainWindow):
         self.timer.start(33)
 
     def paintEvent(self, event):
-        self.draw_notation(r=150, b=150, g=150,
-                           center_x=255, center_y=240, radius=200,
-                           angle=6, radius_in=5)
-        self.draw_notation(r=100, b=100, g=100,
-                           center_x=255, center_y=240, radius=200,
-                           angle=30, radius_in=10)
-        self.draw_gear(r=66, b=133, g=244,
-                       center_x=255, center_y=245,
-                       radius=75, to_left=True)
-        self.draw_gear(r=52, b=168, g=83,
-                       center_x=200, center_y=200,
-                       radius=50, to_left=False)
-        self.draw_gear(r=251, b=188, g=5,
-                       center_x=275, center_y=200,
-                       radius=25, to_left=True)
-        self.draw_gear(r=234, b=67, g=53,
-                       center_x=255, center_y=240,
-                       radius=25, to_left=False)
-        self.draw_arrow(r=100, b=100, g=100,
-                        x=255, y=240, w=5, h=200,
-                        radius=10)
-        self.draw_arrow(r=150, b=150, g=150,
-                        x=255, y=240, w=10, h=150,
-                        radius=60)
-        self.draw_arrow(r=200, b=200, g=200,
-                        x=255, y=240, w=15, h=100,
-                        radius=60*12)
+        self.draw_notation(x_center=255, y_center=240, radius=200,
+                           angle=6, radius_point=5,
+                           r_color=150, g_color=150, b_color=150)
+        self.draw_notation(x_center=255, y_center=240, radius=200,
+                           angle=30, radius_point=10,
+                           r_color=100, g_color=100, b_color=100)
 
-    def draw_arrow(self, r, b, g, x, y, w, h, radius):
-        transform = QtGui.QTransform(1, 0, 0, 1, x, y)
-        transform.rotate(100 / radius * self.rotate)
+        self.draw_gear(x_center=255, y_center=240, radius=60, coefficient=0.015,
+                       n_teeth=15, to_left=True,
+                       r_color=66, g_color=133, b_color=244)
+        self.draw_gear(x_center=255, y_center=240, radius=40, coefficient=0.12,
+                       n_teeth=15, to_left=True,
+                       r_color=251, g_color=188, b_color=5)
+        self.draw_gear(x_center=255, y_center=240, radius=20, coefficient=3.6,
+                       n_teeth=15, to_left=True,
+                       r_color=234, g_color=67, b_color=53)
 
+        self.draw_gear(x_center=320, y_center=255, radius=40, coefficient=0.12,
+                       n_teeth=15, to_left=False,
+                       r_color=52, g_color=168, b_color=83)
+        self.draw_gear(x_center=382, y_center=282, radius=40, coefficient=0.12,
+                       n_teeth=15, to_left=True,
+                       r_color=234, g_color=67, b_color=53)
+
+        self.draw_glass(x_center=255, y_center=240, radius=420,
+                        r_color=200, b_color=200, g_color=200, a_color=50)
+
+        self.draw_arrow(x_center=255, y_center=240, radius=60, coefficient=0.015,
+                        width=12, height=100,
+                        r_color=200, g_color=200, b_color=200)
+        self.draw_arrow(x_center=255, y_center=240, radius=40, coefficient=0.12,
+                        width=6, height=150,
+                        r_color=150, g_color=150, b_color=150)
+        self.draw_arrow(x_center=255, y_center=240, radius=20, coefficient=3.6,
+                        width=5, height=200,
+                        r_color=100, g_color=100, b_color=100)
+
+    def draw_gear(self, x_center, y_center, radius, coefficient, n_teeth, to_left, r_color, g_color, b_color):
+        triangle_points = []
+        for i in range(n_teeth):
+            triangle_points.append([radius * 3 / 4 * np.cos(i * 2 * np.pi / n_teeth),
+                                    radius * 3 / 4 * np.sin(i * 2 * np.pi / n_teeth)]),
+            triangle_points.append([radius * np.cos(i * 2 * np.pi / n_teeth + np.pi / n_teeth),
+                                    radius * np.sin(i * 2 * np.pi / n_teeth + np.pi / n_teeth)]),
+        triangle_points.sort(key=lambda c: np.arctan2(c[0], c[1]), reverse=to_left)
+
+        transform = QtGui.QTransform(1, 0, 0, 1, x_center, y_center)
+        transform.rotate(100 / radius * coefficient * self.rotate * (1 if to_left else -1))
         qp = QtGui.QPainter()
         qp.begin(self)
         qp.setRenderHint(QtGui.QPainter.Antialiasing)
         qp.setTransform(transform)
-        qp.setPen(QtGui.QColor(0, 0, 0, 0))
-        qp.setBrush(QtGui.QColor(r, b, g))
-        qp.drawRect(w / -2, 0, w, -h)
-        qp.end()
-
-    def draw_gear(self, r, b, g, center_x, center_y, radius, to_left):
-        transform = QtGui.QTransform(1, 0, 0, 1, center_x, center_y)
-        transform.rotate(100 / radius * self.rotate * (1 if to_left else -1))
-
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        qp.setRenderHint(QtGui.QPainter.Antialiasing)
-        qp.setTransform(transform)
-        qp.setPen(QtGui.QColor(0, 0, 0, 0))
-        qp.setBrush(QtGui.QColor(r, b, g))
-
-        points_on_circle = []
-        for alpha in range(0, 360, 20):
-            x = radius * np.cos(np.radians(alpha) - np.pi / 2)
-            y = radius * np.sin(np.radians(alpha) - np.pi / 2)
-            points_on_circle.append([x, y])
-
-        points_on_circle.sort(key=lambda c: np.arctan2(c[0], c[1]), reverse=to_left)
-
-        for i in range(len(points_on_circle)):
-            a_point = points_on_circle[i - 1]
-            b_point = points_on_circle[i]
-            bc_line = radius // 5
-            ab_line = np.sqrt((a_point[0] - b_point[0]) ** 2 + (a_point[1] - b_point[1]) ** 2)
-
-            vec_x = (a_point[0] - b_point[0]) / ab_line
-            vec_y = (a_point[1] - b_point[1]) / ab_line
-            c_point = [
-                b_point[0] + ((-vec_y if to_left else vec_y) * bc_line),
-                b_point[1] + ((vec_x if to_left else -vec_x) * bc_line)
-            ]
-            path = QtGui.QPainterPath()
-            path.moveTo(a_point[0], a_point[1])
-            path.lineTo(c_point[0], c_point[1])
-            path.lineTo(b_point[0], b_point[1])
-            qp.fillPath(path, QtGui.QBrush(QtGui.QColor(r, b, g)))
 
         path = QtGui.QPainterPath()
-        path.addText(radius * -1.3, radius * 0.67, QtGui.QFont('Consolas', 1.15 * radius), "⭕")
-        path.addText(radius * -1.3, radius * 0.67, QtGui.QFont('Consolas', 1.15 * radius), "➕")
-        qp.drawPath(path)
-        qp.end()
+        path.clear()
+        for i in triangle_points:
+            path.lineTo(i[0], i[1])
+        path.lineTo(triangle_points[0][0], triangle_points[0][1])
+        path.addEllipse(radius * -0.6, radius * -0.6, radius * 1.2, radius * 1.2)
+        qp.fillPath(path, QtGui.QBrush(QtGui.QColor(r_color, g_color, b_color)))
 
-    def draw_notation(self, r, b, g, center_x, center_y, radius, angle, radius_in):
-        transform = QtGui.QTransform(1, 0, 0, 1, center_x, center_y)
+        path.clear()
+        path.setFillRule(QtCore.Qt.WindingFill)
+        path.addRect(radius / -8, radius / -1.6, radius / 4, radius * 1.3)
+        path.addRect(radius / -1.6, radius / -8, radius * 1.3, radius / 4)
+        path.addEllipse(radius / -4, radius / -4, radius / 2, radius / 2)
+        qp.fillPath(path, QtGui.QBrush(QtGui.QColor(r_color, g_color, b_color)))
+
+    def draw_arrow(self, x_center, y_center, radius, coefficient, width, height, r_color, b_color, g_color):
+        transform = QtGui.QTransform(1, 0, 0, 1, x_center, y_center)
+        transform.rotate(100 / radius * coefficient * self.rotate)
+
         qp = QtGui.QPainter()
         qp.begin(self)
         qp.setRenderHint(QtGui.QPainter.Antialiasing)
         qp.setTransform(transform)
         qp.setPen(QtGui.QColor(0, 0, 0, 0))
-        qp.setBrush(QtGui.QColor(r, b, g))
+        qp.setBrush(QtGui.QColor(r_color, b_color, g_color))
+        qp.drawRect(width / -2, 0, width, -height)
+        qp.end()
+
+    def draw_notation(self, x_center, y_center, radius, angle, radius_point, r_color, b_color, g_color):
+        transform = QtGui.QTransform(1, 0, 0, 1, x_center, y_center)
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing)
+        qp.setTransform(transform)
+        qp.setPen(QtGui.QColor(0, 0, 0, 0))
+        qp.setBrush(QtGui.QColor(r_color, b_color, g_color))
         for alpha in range(0, 360, angle):
             x = radius * np.cos(np.radians(alpha) - np.pi / 2)
             y = radius * np.sin(np.radians(alpha) - np.pi / 2)
-            qp.drawEllipse(x - radius_in / 2, y - radius_in / 2, radius_in, radius_in)
+            qp.drawEllipse(x - radius_point / 2, y - radius_point / 2, radius_point, radius_point)
+
+    def draw_glass(self, x_center, y_center, radius, r_color, b_color, g_color, a_color):
+        transform = QtGui.QTransform(1, 0, 0, 1, x_center, y_center)
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing)
+        qp.setTransform(transform)
+        qp.setPen(QtGui.QColor(0, 0, 0, 0))
+        qp.setBrush(QtGui.QColor(r_color, b_color, g_color, a_color))
+        qp.drawEllipse(radius / -2, radius / -2, radius, radius)
 
     def update_values(self):
         self.rotate += 1
         self.update()
-
-    def draw_meme(self, x, y):
-        pass
 
 
 if __name__ == '__main__':
